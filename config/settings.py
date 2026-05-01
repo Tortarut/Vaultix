75edd1522a297207f61580a52e3c840c5a9e2e12
Tortarut <aleks.safarov2005@gmail.com>
2026-05-01 16:17:25 +0300

    fix docker and add sqlite

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+if os.environ.get("POSTGRES_HOST"):
+    DATABASES["default"] = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB", "vaultix"),
         "USER": os.environ.get("POSTGRES_USER", "vaultix"),
@@ -97,7 +104,6 @@ DATABASES = {
         "HOST": os.environ.get("POSTGRES_HOST", "db"),
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
-}
 
 AUTH_USER_MODEL = "users.User"
 
